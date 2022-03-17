aded09c56cc5d125fc1fbdad4ba362f14ef63a5a
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-03-17 22:40:08 +0530

    imported first component

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <>
+        <nav>
+            <li>Logo here</li>
+            <li>Home</li>
+            <li>About</li>
+            <button>Connect Wallet</button>
+        </nav>
+    </>
+  )
+}
+
+export {Navbar};
