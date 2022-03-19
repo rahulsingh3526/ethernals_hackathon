d937c80e40b845cbfb48996ec297649e0f8368f3
[Kushagra Sarathe] <[kushagrasarathe@gmail.com]>
2022-03-19 13:12:33 +0530

    navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import '../styles/Navbar.css';
+import React from "react";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
     <>
-      <div className='wrapper'>
+      <div className="wrapper">
         <nav>
           <ul class="container">
             <li>Home</li>
             <li>About</li>
-            <li><button>Connect Wallet</button></li>
+            <li>
+              <button className="connectBtn" role="button">
+                Connect Wallet
+              </button>
+            </li>
           </ul>
         </nav>
-
       </div>
     </>
-  )
+  );
 }
 
 export { Navbar };
